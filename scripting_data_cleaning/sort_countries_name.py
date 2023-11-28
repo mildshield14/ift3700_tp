40c83397f753a7620d4f167827f938e816bc95d9
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-11-28 10:23:49 -0500

    added script to facilite country names cleaning and cleaned 21-30 data

---
+++ scripting_data_cleaning/sort_countries_name.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+csv_file_path = '/Users/vennilasooben/Downloads/ift3700_tp/final_cleaned_binarised_data/final_table_21-30 (not done yet).csv'  # Replace with the path to your CSV file
+df = pd.read_csv(csv_file_path)
+
+# Extract the first column with names
+names_column = df.iloc[:, 0]
+
+# Create a dictionary to store data based on the first letter
+data_by_first_letter = {}
+for name in names_column:
+    first_letter = name[0].upper()
+    if first_letter not in data_by_first_letter:
+        data_by_first_letter[first_letter] = []
+    data_by_first_letter[first_letter].append(name)
+
+# Create a new DataFrame based on the dictionary
+new_df = pd.DataFrame(data_by_first_letter.items(), columns=['First Letter', 'Names'])
+
+# Save the new DataFrame to a CSV file
+new_csv_file_path = 'organized_data_country_names.csv'
+new_df.to_csv(new_csv_file_path, index=False)
+
+print(f"Data has been organized and saved to {new_csv_file_path}.")
